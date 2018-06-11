851285151f9854b69936253612ffdaf171018ab8
Claudio Altamura <claudio@claudioaltamura.de>
2018-06-11 09:14:44 +0200

    renamed tests

--- src/test/java/de/claudioaltamura/anony/ip/AnonymizeIpTest.java
+++ src/test/java/de/claudioaltamura/anony/ip/AnonymizeIpTest.java
@@ -12,8 +12,14 @@ class AnonymizeIpTest {
 	}
 	
 	@Test
-	public void testMaskIpV4Address() throws AnonymizeException {
+	public void testAnonymizeIpV4Address() throws AnonymizeException {
 		assertEquals("127.0.0.0", AnonymizeIp.anonymize("127.0.0.1"));
+	}
+	
+	@Test
+	public void testAnonymizeIpV6Address() throws AnonymizeException {
+		//assertEquals("127.0.0.0", AnonymizeIp.anonymize("0:0:0:0:0:ffff:4e32:e0ce"));
+		assertEquals("127.0.0.0", AnonymizeIp.anonymize("2001:0db8:85a3:08d3:1319:8a2e:0370:7344"));
 	}
 
 }
